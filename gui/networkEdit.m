a70538f93764088ec48dfa1d476146ee1223d4d3
ELena Koslover <ekoslover@pc1.koslover.ucsd.edu>
2022-01-31 15:03:43 -0800

    local tweaks

--- gui/networkEdit.m
+++ gui/networkEdit.m
@@ -598,6 +598,12 @@ function pushbuttonAddEdge_Callback(hObject, eventdata, handles)
         hold off
         edgeplotH(i).addprop('edgeind');
         edgeplotH(i).edgeind = i;
+        
+        dttemplate = edgeplotH(i).DataTipTemplate;
+        dttemplate.FontSize=6;
+        dttemplate.DataTipRows(1).Value = i*ones(size(NT.edgepath{i},1),1);
+        dttemplate.DataTipRows(1).Label = '';
+        dttemplate.DataTipRows(2:end) = [];
     catch exception
         disp(getReport(exception))
     end
