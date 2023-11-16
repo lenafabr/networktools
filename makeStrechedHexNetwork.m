e72ea1195b3cfc63e7bc8a90aef4cd67f2048992
Elena Koslover <ekoslover@ucsd.edu>
2023-11-16 11:46:07 -0800

    making stretched hex network

--- makeStrechedHexNetwork.m
+++ makeStrechedHexNetwork.m
@@ -2,31 +2,63 @@
 % structures
 
 %% load in the ATL network regions for comparison
-dirname='../ERCaSims/networks/ATLKO/';
+dirname='../networks/ATLKO/';
 load([dirname,'ATLKO_circreg.mat'])
 
-dirname='../ERCaSims/networks/WT/';
+dirname='../networks/WT/';
 load([dirname,'WT_circreg.mat'])
 
+% --------------
+%% get WT edge lengths
+%% load in WT networks
+dirname='../networks/WT/';
+load([dirname,'WT_circreg.mat'])
+dirname='../networks/ATLKO/';
+load([dirname,'ATLKO_circreg.mat'])
+
+
 %%
-for fc = 1:length(ATLnetworks)
-    avgedgelen(fc) = mean(ATLnetworks(fc).edgelens);
-end
+edgelenWT = [];
 for fc = 1:length(WTnetworks)
-    avgedgelenWT(fc) = mean(WTnetworks(fc).edgelens);
+    NT = copy(WTnetworks(fc));
+    NT.mergeAllEdgePaths(); % merge away degree 2 nodes
+    NT.setCumEdgeLen(1:NT.nedge,true);
+    % pick out only edges between junctions
+    %ind = find(NT.degrees(NT.edgenodes(:,1))> 2 & NT.degrees(NT.edgenodes(:,2))>2);
+    edgelenWT = [edgelenWT; NT.edgelens];
 end
+avglenWT = mean(edgelenWT)
+
+%%
+edgelenATL = [];
+for fc = 1:length(ATLnetworks)
+    NT = copy(ATLnetworks(fc));
+    NT.mergeAllEdgePaths(); % merge away degree 2 nodes
+    NT.setCumEdgeLen(1:NT.nedge,true);
+    %ind = find(NT.degrees(NT.edgenodes(:,1))> 2 & NT.degrees(NT.edgenodes(:,2))>2);
+    edgelenATL = [edgelenATL; NT.edgelens];
+end
+avglenATL = mean(edgelenATL)
+
+
 
 %% load in honeycomb network
-NT0 = NetworkObj('../ERCaSims/networks/circlehexR25N25.net',struct('dim',2));
+NT0 = NetworkObj('../networks/circlehexR25N25.net',struct('dim',2));
+
+% rescale to avg edge len in WT network
+NT0.scaleCoords(avglenWT/mean(NT0.edgelens));
 NT = copy(NT0);
 %%
 NT0.plotNetwork()
 
 %% stretch out the network to an appropriate aspect ratio and rough polygon size
 NTstr = copy(NT0);
-NTstr.nodepos = NTstr.nodepos.*[1.5,3];
+%NTstr.nodepos = NTstr.nodepos.*[1.5,3];
+NTstr.nodepos = NTstr.nodepos.*[1,2.32];
 NTstr.interpolateEdgePaths(2);
 NTstr.setCumEdgeLen(1:NTstr.nedge,true);
+
+%NTstr.scaleCoords(avglenATL/mean(NTstr.edgelens))
 %%
 NTstr.plotNetwork()
 
@@ -34,8 +66,8 @@ NTstr.plotNetwork()
 th = linspace(0,2*pi,50)';
 nodedist = sum(NTstr.nodepos.^2,2);
 [~,centnode] = min(nodedist);
-centpos = NTstr.nodepos(centnode,:);
-cellR = 15; % cell radius
+centpos = NTstr.nodepos(centnode,:)-[0,1];
+cellR = 8; % cell radius
 circbound = centpos + cellR*[cos(th) sin(th)];
 
 NTstr.plotNetwork()
@@ -73,12 +105,13 @@ plot(NT.nodepos(end,1),NT.nodepos(end,2),'r*')
 hold off
 
 %% set node labels and output
+NT.nodelabels = {};
 for nc = 1:NT.nnode
     NT.nodelabels{nc} = '';
 end
 NT.nodelabels{end} = 'P1';
 
-NT.outputNetwork('../ERCaSims/networks/circlehexstretch3.net')
+NT.outputNetwork('../networks/circlehexstretch2pt3_elen1pt4.net')
 
 % -----------
 %% Make a corresponding size network without the stretch
@@ -87,7 +120,7 @@ th = linspace(0,2*pi,50)';
 nodedist = sum(NT0.nodepos.^2,2);
 [~,centnode] = min(nodedist);
 centpos = NT0.nodepos(centnode,:);
-cellR = 15; % cell radius
+cellR = 8; % cell radius
 circbound = centpos + cellR*[cos(th) sin(th)];
 
 [NT, newregbound, boundnodeind, boundedgeind] = breakNetworkRegion(NT0,circbound,struct('keepout',false,'minedgelen',0.3));
@@ -120,9 +153,13 @@ plot(NT.nodepos(end,1),NT.nodepos(end,2),'r*')
 hold off
 
 %% set node labels and output
+NT.nodelabels = {};
 for nc = 1:NT.nnode
     NT.nodelabels{nc} = '';
 end
 NT.nodelabels{end} = 'P1';
 
-NT.outputNetwork('../ERCaSims/networks/circlehexR15deg2P.net')
+NT.outputNetwork('../networks/circlehexR8deg2P_elenpt83.net')
+
+%%
+save('../networks/circlehexstretch2pt3_elen.mat')
